ba064bd25a4d5f8a6cebfc690981b52a6a142c36
Teeermi <145279497+Teeermi@users.noreply.github.com>
2023-12-25 21:07:30 +0100

    Methods

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+    caluclateScore(true, 800, 5, 500);
+
+    }
+
+
+
+public static void caluclateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Twoj wyniki " + finalScore);
+        }
+    }
 }
